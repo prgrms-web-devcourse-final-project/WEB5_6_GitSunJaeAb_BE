9e8c6c5be7528937028f3b10576ffe93fad05241
0802222 <0802222@naver.com>
2025-07-10 09:37:14 +0900

    Revert "(0710) feat: DTO 분리(Response, Request) - 전체 도메인"
    This reverts commit 5faf276be35961ac5bf5c35230c7bbd41545b373.

--- src/main/java/com/gitsunjaeab/mapick/hashtag/HashtagRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gitsunjaeab.mapick.hashtag;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface HashtagRepository extends JpaRepository<Hashtag, Long> {
-}
