b72c90bd7a368b2fa89f9190478a1eec5361ef4e
imseohyeon <forestlight749@gmail.com>
2025-07-11 15:20:39 +0900

    (0711) feat: 데이터 없이 메시지만 반환하는 ApiResponse

---
+++ MAPICK-mocking/src/main/java/com/gitsunjaeab/mapick/api/common/ApiResponse.java
@@ -0,0 +1,23 @@
+package com.gitsunjaeab.mapick.api.common;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ApiResponse implements BaseApiResponse {
+    private String code;
+    private String message;
+    private LocalDateTime timestamp;
+
+    // 데이터 없이 상태코드와 메시지만 반환
+    public static ApiResponse of(ResponseCode responseCode, String message) {
+        return new ApiResponse(
+            responseCode.getCode(),
+            message,
+            LocalDateTime.now()
+        );
+    }
+}
+
