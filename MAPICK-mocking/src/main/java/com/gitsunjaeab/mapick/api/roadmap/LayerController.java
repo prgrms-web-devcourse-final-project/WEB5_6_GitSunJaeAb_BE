a286481eefe50956866e31a239184bf64d65ed41
imseohyeon <forestlight749@gmail.com>
2025-07-13 16:16:07 +0900

    (0713) docs: 레이어 API 설명 추가

--- MAPICK-mocking/src/main/java/com/gitsunjaeab/mapick/api/roadmap/LayerController.java
+++ MAPICK-mocking/src/main/java/com/gitsunjaeab/mapick/api/roadmap/LayerController.java
@@ -2,18 +2,14 @@ package com.gitsunjaeab.mapick.api.roadmap;
 
 import com.gitsunjaeab.mapick.api.roadmap.dto.layer.LayerRequest;
 import com.gitsunjaeab.mapick.application.roadmap.LayerService;
-import com.gitsunjaeab.mapick.api.roadmap.dto.layer.LayerDTO;
-import com.gitsunjaeab.mapick.application.roadmap.LayerLibraryService;
 import com.gitsunjaeab.mapick.common.response.ApiResponse;
 import com.gitsunjaeab.mapick.common.response.ResponseCode;
-import com.gitsunjaeab.mapick.util.ReferencedException;
-import com.gitsunjaeab.mapick.util.ReferencedWarning;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -24,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/layers", produces = MediaType.APPLICATION_JSON_VALUE)
+@Tag(name = "레이어 관리 API")
 public class LayerController {
 
     private final LayerService layerService;
@@ -42,6 +39,7 @@ public class LayerController {
 
     // 레이어 생성
     @PostMapping
+    @Operation(summary = "레이어 생성", description = "[사용자용] 지도 위에 레이어를 생성")
     public ResponseEntity<ApiResponse> createLayer(@RequestBody @Valid final LayerRequest request) {
 //        final Long createdId = layerService.create(layerDTO);
         return ResponseEntity.ok(ApiResponse.of(ResponseCode.OK, "레이어 생성 완료"));
@@ -49,6 +47,7 @@ public class LayerController {
 
     // 특정 레이어 수정
     @PutMapping("/{layerId}")
+    @Operation(summary = "레이어 수정", description = "[사용자용] 특정 레이어를 수정")
     public ResponseEntity<ApiResponse> updateLayer(@PathVariable(name = "layerId") final Long layerId,
             @RequestBody @Valid final LayerRequest request) {
 //        layerService.update(layerId, layerDTO);
@@ -57,6 +56,7 @@ public class LayerController {
 
     // 특정 레이어 삭제
     @DeleteMapping("/{layerId}")
+    @Operation(summary = "레이어 삭제", description = "[사용자용] 특정 레이어 삭제")
     public ResponseEntity<ApiResponse> deleteLayer(@PathVariable(name = "layerId") final Long layerId) {
 //        final ReferencedWarning referencedWarning = layerService.getReferencedWarning(layerId);
 //        if (referencedWarning != null) {
