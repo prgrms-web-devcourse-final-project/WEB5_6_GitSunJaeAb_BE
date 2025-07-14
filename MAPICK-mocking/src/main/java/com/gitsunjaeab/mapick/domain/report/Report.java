26d5cc76d5ced2367e93142e69652fbc45ec2d93
0802222 <0802222@naver.com>
2025-07-14 12:25:09 +0900

    (0714) feat(entity): 모든 엔티티 ID 초기값을 1로 변경

--- MAPICK-mocking/src/main/java/com/gitsunjaeab/mapick/domain/report/Report.java
+++ MAPICK-mocking/src/main/java/com/gitsunjaeab/mapick/domain/report/Report.java
@@ -29,11 +29,11 @@ public class Report {
 
     @Id
     @Column(nullable = false, updatable = false)
-    @SequenceGenerator(
-            name = "primary_sequence",
-            sequenceName = "primary_sequence",
-            allocationSize = 1,
-            initialValue = 10000
+        @SequenceGenerator(
+        name = "primary_sequence",
+        sequenceName = "primary_sequence",
+        allocationSize = 1,
+        initialValue = 1
     )
     @GeneratedValue(
             strategy = GenerationType.SEQUENCE,
