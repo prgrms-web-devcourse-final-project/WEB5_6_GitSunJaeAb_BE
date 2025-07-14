64320f4edb02e7e57d970276835eafc53f41ad28
Seohyeon.Lim <132811149+nunLSH@users.noreply.github.com>
2025-07-14 14:41:33 +0900

    Delete MAPICK-mocking directory

--- MAPICK-mocking/src/main/java/com/gitsunjaeab/mapick/domain/auth/Role.java
+++
@@ -1,5 +0,0 @@
-package com.gitsunjaeab.mapick.domain.auth;
-
-public enum Role {
-    ROLE_ADMIN, ROLE_USER, ANONYMOUS
-}
--- MAPICK-mocking/src/main/java/com/gitsunjaeab/mapick/infra/auth/token/code/GrantType.java
+++
@@ -1,5 +0,0 @@
-package com.gitsunjaeab.mapick.infra.auth.token.code;
-
-public enum GrantType {
-    BEARER, BASIC, DIGEST, HOBA, MUTUAL;
-}
