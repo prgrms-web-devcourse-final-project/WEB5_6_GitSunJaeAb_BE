0259087d296cd98ee44cd72b34111ee34010af32
0802222 <0802222@naver.com>
2025-07-22 09:13:34 +0900

    (0721) feat : 알림-전체, 찜, 포크 알림 GET API 구현(09:12)

---
+++ MAPICK/src/main/java/com/gitsunjaeab/mapick/api/notification/dto/NotificationForkListResponse.java
@@ -0,0 +1,57 @@
+package com.gitsunjaeab.mapick.api.notification.dto;
+
+import com.gitsunjaeab.mapick.api.member.dto.MemberSimpleDTO;
+import com.gitsunjaeab.mapick.api.roadmap.dto.RoadmapSimpleDTO;
+import com.gitsunjaeab.mapick.api.roadmap.dto.roadmap.RoadmapListResponse;
+import com.gitsunjaeab.mapick.common.response.BaseApiResponse;
+import com.gitsunjaeab.mapick.common.response.ResponseCode;
+import com.gitsunjaeab.mapick.domain.notification.Notification;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class NotificationForkListResponse implements BaseApiResponse {
+
+    private String code;
+    private String message;
+    private LocalDateTime timestamp;
+    private List<NotificationForkListDTO> notifications;
+
+    // 공통 처리 로직
+    public static NotificationForkListResponse of(List<Notification> notificationEntities) {
+        return of(notificationEntities, "알림 목록 조회 성공");
+    }
+
+    // 생성 패턴용
+    public static NotificationForkListResponse of(List<Notification> notificationEntities, String message) {
+        List<NotificationForkListDTO> notificationForkListDTOs = notificationEntities.stream()
+            .map(n -> {
+                NotificationForkListDTO dto = new NotificationForkListDTO();
+                dto.setId(n.getId());
+                dto.setTitle(n.getTitle());
+                dto.setContent(n.getContent());
+                dto.setMember(n.getMember() != null ? new MemberSimpleDTO(n.getMember()) : null);
+                dto.setCreatedAt(n.getCreatedAt());
+                dto.setUpdatedAt(n.getUpdatedAt());
+                dto.setDeletedAt(n.getDeletedAt());
+                dto.setReadAt(n.getReadAt());
+                dto.setNotificationType(n.getNotificationType());
+                dto.setAnnouncementType(n.getAnnouncementType());
+                dto.setRead(n.isRead());
+                dto.setRoadmapSimpleDTO(n.getRoadmap() != null ? new RoadmapSimpleDTO(n.getRoadmap()) : null);
+                return dto;
+            })
+            .toList();
+
+        return new NotificationForkListResponse(
+            ResponseCode.OK.getCode(),
+            message,
+            LocalDateTime.now(),
+            notificationForkListDTOs
+        );
+
+    }
+}
---
+++ MAPICK/src/main/java/com/gitsunjaeab/mapick/domain/notification/Notification.java
@@ -0,0 +1,124 @@
+package com.gitsunjaeab.mapick.domain.notification;
+
+import com.gitsunjaeab.mapick.common.response.ResponseCode;
+import com.gitsunjaeab.mapick.domain.member.Member;
+import com.gitsunjaeab.mapick.domain.quest.MemberQuest;
+import com.gitsunjaeab.mapick.domain.roadmap.LayerLibrary;
+import com.gitsunjaeab.mapick.domain.roadmap.Roadmap;
+import com.gitsunjaeab.mapick.infra.converter.OffsetDateTimeConverter;
+import com.gitsunjaeab.mapick.infra.error.exceptions.CommonException;
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import java.time.OffsetDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "Notifications")
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(onlyExplicitlyIncluded = true)
+public class Notification {
+
+    @Id
+    @Column(nullable = false, updatable = false)
+    @SequenceGenerator(
+        name = "primary_sequence",
+        sequenceName = "primary_sequence",
+        allocationSize = 1,
+        initialValue = 100
+    )
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "primary_sequence"
+    )
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    @ToString.Include
+    private Member member;
+
+    @Column(nullable = false)
+    private boolean isRead = false; // 기본값 : 읽지 않음
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = true, name = "announcement_type")
+    private AnnouncementType announcementType; // 공지 타입
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, name = "notification_type")
+    private NotificationType notificationType; // 알림 타입
+
+    @CreatedDate
+    @Convert(converter = OffsetDateTimeConverter.class)
+    @Column(nullable = false, updatable = false)
+    private OffsetDateTime createdAt;
+
+    @LastModifiedDate
+    @Convert(converter = OffsetDateTimeConverter.class)
+    @Column
+    private OffsetDateTime updatedAt;
+
+    @Convert(converter = OffsetDateTimeConverter.class)
+    @Column
+    private OffsetDateTime deletedAt;
+
+    @Convert(converter = OffsetDateTimeConverter.class)
+    @Column
+    private OffsetDateTime readAt;
+
+    // 연관 관계
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "roadmap_id")
+    private Roadmap roadmap;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "layer_library_id")
+    private LayerLibrary layerLibrary;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_quest_id")
+    private MemberQuest memberQuest;
+
+
+    // ===== UTIL =====
+    // Enum 값 저장 검증 로직
+    @PrePersist
+    private void validateBeforeSave() {
+        if (notificationType == NotificationType.ALL) {
+            throw new CommonException(ResponseCode.SAVE_FAILED, "NotificationType.All 은 저장할 수 없습니다.");
+        }
+    }
+
+}
