64b3c4a159eca2a0c0d6042486bbd14325c92add
moonbong <[GitHub>
2025-07-30 09:55:13 +0900

    (0730) refactor: 공유지도 삭제 -> 관리자 권한으로 삭제 기능 수정

--- MAPICK/src/main/java/com/gitsunjaeab/mapick/domain/roadmap/RoadmapRepository.java
+++ MAPICK/src/main/java/com/gitsunjaeab/mapick/domain/roadmap/RoadmapRepository.java
@@ -13,18 +13,11 @@ public interface RoadmapRepository extends JpaRepository<Roadmap, Long> {
 
     Roadmap findFirstByMember(Member member);
 
-    // 마이페이지 - 회원별 지도 조회용
-    List<Roadmap> findByMember(Member member);
-
-//    Roadmap findFirstByOriginalRoadmapAndIdNot(
-//        Roadmap roadmap, final Long id);
-
     List<Roadmap> findAllByIsPublicTrueAndRoadmapTypeAndCategoryId(RoadmapType roadmapType, Long categoryId);
 
     List<Roadmap> findAllByMember_IdAndDeletedAtIsNull(Long memberId);
 
     Optional<Roadmap> findByIdAndDeletedAtIsNull(Long roadmapId);
-    List<Roadmap> findAllByMemberAndDeletedAtIsNull(Member member);
 
     Long countByMemberId(Long memberId);
 
